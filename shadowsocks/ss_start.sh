6c0d5b9432036476eee9fa1be12c25666594b8a3
someoneonsmile <1569275656@qq.com>
2018-07-07 14:57:11 +0800

    shadowsockets docker

---
+++ shadowsocks/ss_start.sh
@@ -0,0 +1,2 @@
+docker run -dt --name ssserver -p 6443:6443 -p 6500:6500/udp mritd/shadowsocks -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m aes-256-cfb -k smile --fast-open" -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2"
+docker run -dt --name ssserver2 -p 6403:6403 -p 6501:6501/udp mritd/shadowsocks -m "ss-server" -s "-s 0.0.0.0 -p 6403 -m aes-256-cfb -k smile --fast-open" -x -e "kcpserver" -k "-t 127.0.0.1:6403 -l :6501 -mode fast2"
